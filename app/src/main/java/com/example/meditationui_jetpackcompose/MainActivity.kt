cdc4c1a09adce78a719996959268734e98a2a897
vivekbargude <vivekbargude1206@gmail.com>
2025-07-06 23:41:53 +0530

    day 1

---
+++ app/src/main/java/com/example/meditationui_jetpackcompose/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.meditationui_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditationui_jetpackcompose.ui.theme.HomeScreen
+import com.example.meditationui_jetpackcompose.ui.theme.MeditationUI_JetPackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MeditationUI_JetPackComposeTheme {
+
+            }
+        }
+    }
+}
+

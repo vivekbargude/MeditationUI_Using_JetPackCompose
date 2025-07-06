9a1c1ec29ec3e4e8647f9c65ca147c4fe57fbf69
vivekbargude <vivekbargude1206@gmail.com>
2025-07-06 23:42:55 +0530

    day 1

--- app/src/main/java/com/example/meditationui_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/meditationui_jetpackcompose/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MeditationUI_JetPackComposeTheme {
-
+                HomeScreen()
             }
         }
     }
